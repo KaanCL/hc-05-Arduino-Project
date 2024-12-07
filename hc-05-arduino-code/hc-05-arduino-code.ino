7cb5ef0078c1366d96eaea02e6195c6b48012f44
KaanCL <Kaancerkez34@outlook.com>
2024-12-07 16:10:49 +0300

    at komutu ile buzzer kontrolü sağlandı

--- hc-05-arduino-code/hc-05-arduino-code.ino
+++ hc-05-arduino-code/hc-05-arduino-code.ino
@@ -1,38 +1,65 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(0,1); // RX || TX
+SoftwareSerial BTSerial(10, 11); // RX || TX
 
 int gazSensorPin = A0;
 int sensorValue = 0;
 int buzzerPin = 8;
+int redLedPin = 12; 
+int greenLedPin= 13;
+
+boolean alarm = false;
+boolean stop = false;
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+
   Serial.begin(9600); // Serial Monitor iletişimi için
-  BTSerial.begin(38400); // Bluetooth modülünün baud hızını ayarlıyoruz
+  BTSerial.begin(9600); // Bluetooth modülünün baud hızını ayarlıyoruz
   Serial.println("Bluetooth ile iletişim başlatıldı.");
 }
 
 void loop() {
   // Gaz sensöründen veri oku
   sensorValue = analogRead(gazSensorPin);
+
   delay(1000);
 
+  Serial.println(alarm);
+  Serial.println(sensorValue);
+
   // Gaz sensörü değeri 100'ün üzerinde ise buzzeri çalıştır
-  if (sensorValue >= 100) {
+  alarm = sensorValue >= 100 ? true : false;
+
+  if (alarm && !stop) {
+    digitalWrite(greenLedPin, LOW);
     digitalWrite(buzzerPin, HIGH);
-    delay(1000);
+    digitalWrite(redLedPin, HIGH);
+  } else {
+    digitalWrite(greenLedPin, HIGH);
     digitalWrite(buzzerPin, LOW);
+    digitalWrite(redLedPin, LOW); 
   }
 
   // Bluetooth modülünden gelen veriyi Serial Monitor'e yazdır
   if (BTSerial.available()) {
-    Serial.write(char(BTSerial.read()));
-    
+    char gelenVeri = BTSerial.read();   
+    if(alarm){
+     if (gelenVeri == 's') {
+      stop = true; // Alarmı kapat
+      BTSerial.print("Alarm kapatıldı\n");
+    }
+    }else{stop = false;}
+   
+    Serial.write(gelenVeri);
   }
 
   // Serial Monitor üzerinden gelen veriyi Bluetooth modülüne gönder
   if (Serial.available()) {
-    BTSerial.write(Serial.read()); // Sensör değerini Bluetooth'a gönder
+    char gidenVeri = Serial.read();   
+    BTSerial.write(gidenVeri);
   }
+
 }
